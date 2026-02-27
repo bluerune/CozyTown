5f1cd1fb484aba579d3594e1386177e947d09443
bluerune <128613465+bluerune@users.noreply.github.com>
2026-02-27 04:27:28 -0300

    update

--- script/navbar.js
+++ script/navbar.js
@@ -3,13 +3,11 @@ navbar = document.getElementById("navbar");
 navbar.innerHTML = `
         <a href="index.html"><img src="img/navbar/cozytowndotcom_logo.png" class="cozytownLogoNavbar" alt="CozyTown.com"></a>
         <div class="cloudsBanner"> 
-            <a href="closet.html"><div class="profilepictureNavbar" alt=""></div></a>
-            <p class="welcomeNavbar">Welcome, <a href="user-profile.html">username</a>!</p>
-            <img class="iconsNavbar" src="img/navbar/star.png" alt=""> <a href="world-map.html">
-                Explore</a>
-            <img class="iconsNavbar" src="img/navbar/star.png" alt=""> <a href="forums.html">
-                Forums</a>
-            <img class="iconsNavbar" src="img/navbar/star.png" alt=""> <a href="inbox.html">
-                Cozymail</a>
+            
+            <p class="welcomeNavbar">Welcome, <a href="my-profile.html">username</a>!</p>
+            <img class="iconsNavbar" src="img/navbar/star.png"><a href="world-map.html">Explore</a>
+            <img class="iconsNavbar" src="img/navbar/star.png"><a href="inbox.html">Cozymail</a>
+            <img class="iconsNavbar" src="img/navbar/star.png"><a href="forums.html">Forums</a>
+            <img class="iconsNavbar" src="img/navbar/star.png"><a href="closet.html">Closet</a>
         </div>
 `;
