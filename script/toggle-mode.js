4b1331f86255dbb641c63189ba15472f6a5e0785
bluerune <128613465+bluerune@users.noreply.github.com>
2026-03-01 04:45:22 -0300

    bugfix

--- script/toggle-mode.js
+++ script/toggle-mode.js
@@ -2,16 +2,14 @@ const themeStyle = document.createElement('link');
 themeStyle.rel = 'stylesheet';
 document.head.appendChild(themeStyle);
 
-// Cargar el tema preferido
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
-    themeStyle.href = 'styles/dark-mode.css'; // Ruta a tu CSS de modo oscuro
+    themeStyle.href = 'styles/dark-mode.css'; 
     document.getElementById('toggleTheme').textContent = 'Light Mode';
 } else {
-    themeStyle.href = 'styles/light-mode.css'; // Ruta a tu CSS de modo claro
+    themeStyle.href = 'styles/light-mode.css'; 
 }
 
-// Evento para el botón de cambio de tema
 document.getElementById('toggleTheme').addEventListener('click', function () {
     if (themeStyle.href.includes('light-mode.css')) {
         themeStyle.href = 'styles/dark-mode.css';
