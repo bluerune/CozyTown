bce52a1315afa320344de63a249ca2eb6cc9168e
bluerune <128613465+bluerune@users.noreply.github.com>
2026-03-05 00:43:10 -0300

    inbox to cozymail

--- script/navbar.js
+++ script/navbar.js
@@ -7,7 +7,7 @@ navbar.innerHTML = `
             
             <p class="welcomeNavbar">Welcome, <a href="my-profile.html">username</a>!</p>
             <img class="iconsNavbar" src="img/navbar/star.png"><a href="world-map.html">Explore</a>
-            <img class="iconsNavbar" src="img/navbar/star.png"><a href="inbox.html">Cozymail</a>
+            <img class="iconsNavbar" src="img/navbar/star.png"><a href="cozymail.html">Cozymail</a>
             <img class="iconsNavbar" src="img/navbar/star.png"><a href="forums.html">Forums</a>
             <img class="iconsNavbar" src="img/navbar/star.png"><a href="closet.html">Closet</a>
         </div>
