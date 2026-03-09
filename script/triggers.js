6648f5cfa569ab200bfaeca43563eb4a4c66b214
bluerune <128613465+bluerune@users.noreply.github.com>
2026-03-09 01:13:28 -0300

    wip bg map

---
+++ script/triggers.js
@@ -0,0 +1,4 @@
+// 1 entrar al hospital
+// 2 entrar al perfil de jo
+// 3 cabina de fotos en el parque tiene tu foto con la de jo y dm de cherrypopart
+//si intentas bloquear a jo se glitchea y te dice que pagaras las consecuencias
